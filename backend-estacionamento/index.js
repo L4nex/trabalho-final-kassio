c56b98af5bfa6a0b9de0535e9cb43d86e6e748ba
L4nex <mchadolucas@gmail.com>
2021-06-30 20:37:36 -0300

    CRUD de rotativos finalizado

--- backend-estacionamento/index.js
+++ backend-estacionamento/index.js
@@ -3,12 +3,13 @@ const express = require("express");
 const cors = require("cors");
 const usuariosRoute = require("./routes/usuariosRoute");
 const mensalistasRoute = require('./routes/mensalistasRoute');
+const rotativosRoute = require('./routes/rotativosRoute');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(usuariosRoute);
 app.use(mensalistasRoute);
-
+app.use(rotativosRoute);
 
 app.listen(3333);
