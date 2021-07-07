546a88f321d5f9d2826d52945968289895f6f726
L4nex <mchadolucas@gmail.com>
2021-07-07 20:42:45 -0300

    login, logout e validação JWT

--- backend-estacionamento/routes/veiculosRoute.js
+++ backend-estacionamento/routes/veiculosRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const veiculosService = require("../service/veiculosService");
+const jwt = require('../jwtValidator');
 
-router.post("/mensalista/:idMensalista/veiculo", async function (req, res) {
+router.post("/mensalista/:idMensalista/veiculo",jwt.validator, async function (req, res) {
   const veiculo = req.body.veiculo;
   const idMensalista = req.params.idMensalista;
   const novoVeiculo = await veiculosService.criarVeiculo(veiculo, idMensalista);
   res.json({ message: "Veiculo criado com sucesso!", id: novoVeiculo.id })
 });
 
-router.put("/mensalista/:idMensalista/veiculo/:id", async function (req, res) {
+router.put("/mensalista/:idMensalista/veiculo/:id",jwt.validator, async function (req, res) {
   const idVeiculo = req.params.id;
   const veiculo = req.body.veiculo;
   const alterarVeiculo = await veiculosService.alterarVeiculo(veiculo, idVeiculo);
@@ -17,19 +18,19 @@ router.put("/mensalista/:idMensalista/veiculo/:id", async function (req, res) {
   res.json(alterarVeiculo);
 });
 
-router.delete("/mensalista/:idMensalista/veiculo/:id", async function (req, res) {
+router.delete("/mensalista/:idMensalista/veiculo/:id",jwt.validator, async function (req, res) {
   const idVeiculo = req.params.id;
   await veiculosService.deletarVeiculo(idVeiculo);
   res.json({ message: "Veiculo deletado com sucesso!", id: idVeiculo })
 });
 
-router.patch("/mensalista/:idMensalista/veiculo/:id/recuperar", async function (req, res) {
+router.patch("/mensalista/:idMensalista/veiculo/:id/recuperar",jwt.validator, async function (req, res) {
   const idVeiculo = req.params.id;
   await veiculosService.recuperarVeiculo(idVeiculo);
   res.json({ message: "Veiculo restaurado com sucesso!", id: idVeiculo })
 });
 
-router.get("/mensalista/:idMensalista/veiculo/:id", async function (req, res) {
+router.get("/mensalista/:idMensalista/veiculo/:id",jwt.validator, async function (req, res) {
   const idVeiculo = req.params.id;
   const veiculo = await veiculosService.mostrarVeiculo(idVeiculo);
     if (veiculo != ""){ 
@@ -39,7 +40,7 @@ router.get("/mensalista/:idMensalista/veiculo/:id", async function (req, res) {
   } 
 });
 
-router.get("/mensalista/:idMensalista/veiculos", async function (req, res) {
+router.get("/mensalista/:idMensalista/veiculos",jwt.validator, async function (req, res) {
   const idMensalista = req.params.idMensalista;
   const veiculos = await veiculosService.listarVeiculos(idMensalista);
   res.json(veiculos)
