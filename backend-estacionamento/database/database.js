8e1857c876873942f17c86eb6993998641b7525e
L4nex <mchadolucas@gmail.com>
2021-07-08 14:58:59 -0300

    package json e database

--- backend-estacionamento/database/database.js
+++ backend-estacionamento/database/database.js
@@ -1,11 +1,7 @@
 const pgp = require('pg-promise')();
 const db = pgp({
     user: 'postgres',
-<<<<<<< HEAD
     password: 'Senha123',
-=======
-    password: '1234',
->>>>>>> 21d829f77c01592f2dd12d82902eb1138ad54801
     host: 'localhost',
     port: '5432',
     database: 'estacionamento'
