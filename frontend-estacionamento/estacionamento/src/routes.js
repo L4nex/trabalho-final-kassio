598b42665921001ed078ca4697f9ddfae5618515
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2021-07-07 21:24:04 -0300

    aa

--- frontend-estacionamento/estacionamento/src/routes.js
+++ frontend-estacionamento/estacionamento/src/routes.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import CadastroMensalista from "./components/CadastroMensalista/CadastroMensalista";
+import CadastroRotativo from "./components/CadastroRotativo/CadastroRotativo";
 import CadastroUsuario from "./components/CadastroUsuario";
 import CadastroVeiculo from "./components/CadastroVeiculo";
 import ListarMensalistas from "./components/ListarMensalistas";
+import ListarRotativos from "./components/ListarRotativos";
 import ListarUsuarios from "./components/ListarUsuarios";
 import Sorteador from "./padrao";
 
@@ -13,10 +15,12 @@ function Routes() {
       <Switch>
         <Route path="/" exact={true} component={Sorteador} />
         <Route path="/cadastrarUsuarios" component={CadastroUsuario} />
+        <Route path="/listarRotativos" component={ListarRotativos} />
         <Route path="/listarUsuarios" component={ListarUsuarios} />
         <Route path="/cadastrarMensalistas" component={CadastroMensalista} />
         <Route path="/cadastrarVeiculos" component={CadastroVeiculo} />
         <Route path="/listarMensalistas" component={ListarMensalistas} />
+        <Route path="/cadastrarRotativo" component={CadastroRotativo} />
       </Switch>
     </BrowserRouter>
   );
